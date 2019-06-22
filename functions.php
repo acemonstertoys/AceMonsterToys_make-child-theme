77d8308a0345b84f191f864b28d517b1f7ac7ab0
Aaron Barraza <aaron@mightyminnow.com>
2019-06-22 09:00:59 -0700

    Send more order data to Stripe

--- functions.php
+++ functions.php
@@ -1471,3 +1471,20 @@ add_action( 'wp_enqueue_scripts', function(){
 			'5.0.13'
 		);
 }, 20 );
+
+function filter_wc_stripe_payment_metadata( $metadata, $order, $source ) {		
+    $count = 1;
+	foreach( $order->get_items() as $item_id => $line_item ){
+		$item_data = $line_item->get_data();
+		$product = $line_item->get_product();
+		$product_name = $product->get_name();
+		$item_quantity = $line_item->get_quantity();
+		$item_total = $line_item->get_total();
+		$metadata['Line Item '.$count] = 'Product name: '.$product_name.' | Quantity: '.$item_quantity.' | Item total: '. number_format( $item_total, 2 );
+		$count += 1;
+	}
+
+	return $metadata;
+}
+
+add_filter( 'wc_stripe_payment_metadata', 'filter_wc_stripe_payment_metadata', 10, 3 );
